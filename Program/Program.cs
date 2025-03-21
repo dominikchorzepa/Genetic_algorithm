6e2b67b73340f62b25a584707c048ecd43c07371
Dominik Chorzępa <dominikchorzepa@wp.pl>
2025-03-21 21:07:30 +0100

    kodowanie

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,9 +1,53 @@
 ﻿namespace Program;
 
+class Parametr
+{
+    public int Wartosc {get; set;}
+    public double Funkcja {get; set;}
+
+    public Parametr(int wartosc, double funkcja){
+        Wartosc = wartosc;
+        Funkcja = funkcja;
+    }
+
+    public void Wyswietl(){
+        Console.WriteLine("Parametr: " + Wartosc + " " + "Funkcja: " + Funkcja);
+    }
+}
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Random rand = new Random();
+
+        double pm = 5;
+        int ZDMin = -1;
+        int ZDMax = 2;
+        int LBnP = 3;
+        int ZD = ZDMax - ZDMin;
+        int[] cb = new int[3];
+
+    
+        pm = Math.Max(pm, ZDMin);
+        pm = Math.Min(pm, ZDMax);
+
+        double ctmp = Math.Round(((pm - ZDMin) / ZD) * (Math.Pow(2, LBnP) - 1));
+
+        for (int b = 0; b < LBnP; b++)
+        {
+            cb[b] = (int)Math.Floor(ctmp / Math.Pow(2, b)) % 2;
+        }
+
+        Array.Reverse(cb);
+
+        foreach (int b in cb)
+        {
+            Console.Write(b);
+        }
+
+        
+
+        
     }
 }
